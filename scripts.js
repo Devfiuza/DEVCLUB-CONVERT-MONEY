cc85f652b47c0df7da6cd8ce87bb282cc6004fb2
Devfiuza <eduardo.costagomes@hotmail.com>
2025-03-03 16:28:49 -0300

    adição de novas moedas (libra e bitcoin)

--- scripts.js
+++ scripts.js
@@ -11,8 +11,10 @@ function convertValues() {
   const currencyValueConverted = document.querySelector(".currency-value"); //outras moedas
 
   console.log(currencySelect.value);
-  const dolarToday = 5.2;
-  const euroToday = 6.2;
+  const dolarToday = 5.2
+  const euroToday = 6.2
+  const libraToday=7.4
+  const bitcoinToday=523.298
 
   if (currencySelect.value == "dolar") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -24,13 +26,26 @@ function convertValues() {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "EUR",
-    }).format(inputCurrencyValue / euroToday);
+    }).format(inputCurrencyValue / euroToday)
+
+  } if (currencySelect.value == "libra") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP"
+    }).format(inputCurrencyValue / libraToday);
+  }
+  if (currencySelect.value == "bitcoin") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "BTC",
+    }).format(inputCurrencyValue / bitcoinToday)
   }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(inputCurrencyValue);
+  }).format(inputCurrencyValue)
+
 }
 
 function changeCurrency() {
@@ -41,14 +56,20 @@ function changeCurrency() {
   currencyName.innerHTML="Dólar americano"
   currencyImage.src="./assets/dolar.png"
  
-  
  }
 
   if(currencySelect.value=="euro") {
   currencyName.innerHTML="Euro"
   currencyImage.src="./assets/euro.png"
   
-
+}
+if(currencySelect.value=="libra") {
+  currencyName.innerHTML="Libra"
+  currencyImage.src="./assets/libra.png"
+  }
+  if(currencySelect.value=="bitcoin") {
+    currencyName.innerHTML="Bitcoin"
+    currencyImage.src="./assets/bitcoin.png"
 }
 convertValues()
 }
